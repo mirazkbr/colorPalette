697a9d55a7f77caa23d2dc0a0e9e220cade04e55
mirazkbr <83450502+mirazkbr@users.noreply.github.com>
2024-02-24 21:23:26 +0600

    Update ColorPalette.jsx

--- src/component/ColorPalette.jsx
+++ src/component/ColorPalette.jsx
@@ -14,7 +14,7 @@ const ColorPalette = () => {
 
     const fetchColors = async () => {
         try {
-            const response = await axios.get('color-api-blond.vercel.app/colors');
+            const response = await axios.get('https://color-api-xgao.onrender.com/mycolors');
             setColors(response.data);
         } catch (error) {
             console.error('Error fetching colors:', error);
@@ -25,7 +25,7 @@ const ColorPalette = () => {
         try {
             // Check if newColor starts with #
             const colorToAdd = newColor.startsWith('#') ? newColor : `#${newColor}`;
-            await axios.post('color-api-blond.vercel.app/colors', { color: colorToAdd }); // Send colorToAdd to the server
+            await axios.post('https://color-api-xgao.onrender.com/colors', { color: colorToAdd }); // Send colorToAdd to the server
             fetchColors(); // Fetch updated colors after adding
             setNewColor(''); // Clear input after adding
             toast.success("Color added successfully!"); // Show toast notification
